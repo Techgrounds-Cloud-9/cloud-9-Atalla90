f26d545c1dbbe1cb6cdf9e819bfecdafdbc99ed5
Atalla90 <106679524+Atalla90@users.noreply.github.com>
2022-10-13 12:32:53 +0200

    Create Data types and comments_1.py

---
+++ 04_Python_1/Data types and comments_1.py
@@ -0,0 +1,16 @@
+#Copying the variables and their initial values as stated in the exercise
+a = "int"
+b = 7
+c = False
+d = "18.5"
+
+#Printing the type of each current value contained in each variable using the type() method
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
+
+#Creating the variable x that contains the value of b + d using the float() and printing its output
+x = b + float(d)
+print (x)
+
