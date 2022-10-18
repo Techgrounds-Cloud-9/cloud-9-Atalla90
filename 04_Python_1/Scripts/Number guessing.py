7db9aa110650a3164c4bdb4883bd205f7708f0b2
Atalla90 <106679524+Atalla90@users.noreply.github.com>
2022-10-18 11:26:27 +0200

    Update Number guessing.py

--- 04_Python_1/Scripts/Number guessing.py
+++ 04_Python_1/Scripts/Number guessing.py
@@ -1,22 +1,34 @@
 import random
 
-x = random.randint(1,100)
-y = input("Guess the number between 1 and 100\n")
-y = int(y)
+def game():
+    x = random.randint(1,100)
+    y = input("Guess the number between 1 and 100\n")
+    y = int(y)
 
-while y > 100 or y < 1:
-        y = input("Please enter a number between 1 and 100!\n")
-        y = int(y)
+    while y > 100 or y < 1:
+            y = input("Please enter a number between 1 and 100!\n")
+            y = int(y)
 
+    while True:
+            if y == x:
+                print("That's true! The number is " + str(x))
+                break
+            elif y < x:
+                y = input("Try higher. Don't be afraid!\n")
+                y = int(y)
+                continue
+            else:
+                y = input("Try lower. Don't be so greedy!\n")
+                y = int(y)
+                continue
+
+game()
 while True:
-        if y == x:
-            print("That's true! The number is " + str(x))
-            break
-        elif y < x:
-            y = input("Try higher. Don't be afraid!\n")
-            y = int(y)
-            continue
-        else:
-            y = input("Try lower. Don't be so greedy!\n")
-            y = int(y)
-            continue
+    restart = input("Play again? Type 'yes' or 'no'\n")
+    if restart == "yes" or restart == "Yes" or restart == "y" or restart == "Y":
+        game()
+    elif restart == "no" or restart == "No" or restart == "n" or restart == "N":
+        break
+    else:
+        print("This is not a valid entry!")
+
