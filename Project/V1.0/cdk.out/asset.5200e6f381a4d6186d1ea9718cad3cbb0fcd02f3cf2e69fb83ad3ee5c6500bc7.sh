369f5c44ec28be98c9b0d62dd2f58c742fd1edf0
Atalla90 <106679524+Atalla90@users.noreply.github.com>
2022-12-16 10:59:01 +0100

    Delete Project directory

--- Project/V1.0/cdk.out/asset.5200e6f381a4d6186d1ea9718cad3cbb0fcd02f3cf2e69fb83ad3ee5c6500bc7.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yum -y install httpd
-systemctl enable httpd
-systemctl start httpd
-echo '<html><h1>That's one small step for a man, one giant leap for the same man</h1></html>' > /var/www/html/index.html
